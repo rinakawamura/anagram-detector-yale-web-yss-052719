100365caac775fa3571c4621cca83bd68bfab977
rinakawamura <rina.kawamura@yale.edu>
2019-05-30 13:33:48 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+class Anagram 
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        matches = []
+        array.select do |anagram|
+            add = true
+            letters = @word.split("")
+            for i in 0...anagram.length do
+                if !letters.include?(anagram[i])
+                    add = false
+                else
+                    letters.delete_at(letters.index(anagram[i]))
+                end
+            end
+            if add 
+                matches << anagram
+            end
+        end
+        return matches
+    end              
+end
